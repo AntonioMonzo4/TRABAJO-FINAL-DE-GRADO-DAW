e9a4b94ed4680b27e6b7c4e3ef1bddbf7b7343f4
AntonioMonzo <amonansu@gmail.com>
2025-11-15 22:10:14 +0100

    index.php + rutas.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+
+// Habilitar la visualización de errores para depuración
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Incluir el archivo de rutas para manejar las solicitudes
+require_once 'CONTROLLER/rutas.php';
