0ff200a393a7b60232544aef439a7230ee214c08
AntonioMonzo <amonansu@gmail.com>
2025-11-16 12:59:14 +0100

    MOD footer TODO: enlazar bien y colocar

---
+++ VIEW/footer.php
@@ -0,0 +1,46 @@
+<footer class="footer">
+    <img src="./VIEW/img/logo_principal.png" alt="logo_footer">
+
+    <div class="centerFooter">
+        <h3>Secciones</h3>
+        <ul>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Tienda</a></li>
+            <li><a href="">Sobre Nosotros</a></li>
+            <li><a href="">Contacto</a></li>
+        </ul>
+
+        <h3>Legal</h3>
+        <ul>
+            <li><a href="">Política de Privacidad</a></li>
+            <li><a href="">Términos y Condiciones</a></li>
+            <li><a href="">Cookies</a></li>
+        </ul>
+    </div>
+
+    <div class="rightFooter">
+        <h3>Contacto</h3>
+        <p>Dirección: Calle Falsa 123, Ciudad, País</p>
+        <p>Teléfono: +34 123 456 789</p>
+        <p>Email: contacto@ejemplo.com</p>
+
+        <div class="socialMedia">
+            <a href="" title="Facebook"><i></i></a>
+            <a href="" title="Twitter"><i></i></a>
+            <a href="" title="Instagram"><i></i></a>
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+    </body>
+</html>
