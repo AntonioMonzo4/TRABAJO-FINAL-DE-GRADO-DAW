5a7f7b7562117f6e6eab5d0574995406494de254
AntonioMonzo <amonansu@gmail.com>
2025-11-17 22:57:57 +0100

    MOD producto

--- MODEL/user.php
+++ MODEL/user.php
@@ -11,6 +11,7 @@ class User {
         $this->conexion = Conexion::conexionBBDD();
     }
 
+    //Método para el login de usuario
     public function login($email,$password){
 
         //Sentencia sql para que se busque al usuario introducido 
@@ -20,6 +21,7 @@ class User {
         $stmt = $this->conexion->prepare($sql);
     }
 
+    //Método para el registro de usuario
     public function register ($data){
 
         if($this->emailExists($data['email'])){
@@ -30,8 +32,10 @@ class User {
         $sql = "INSERT INTO {$this->tabla} (nombre, apellidos, email, password_hash, fecha_nacimiento, telefono, genero, rol) VALUES (:nombre, :apellidos, :email, :password_hash, :fecha_nacimiento, :telefono, :genero, 'cliente')";
 
         try{
+            // Preparar la sentencia SQL 
             $stmt = $this->conexion->prepare($sql);
 
+            // Vincular los parámetros
             $stmt->bindParam(':nombre', $data['nombre']);
             $stmt->bindParam(':apellidos', $data['apellidos']);
             $stmt->bindParam(':email', $data['email']);
@@ -51,6 +55,7 @@ class User {
     }
 
 
+    //Método para comprobar si el email ya existe en la base de datos
     public function emailExists($email){
         $sql = "SELECT COUNT(*) FROM {$this->tabla} WHERE email = :email";
         $stmt = $this->conexion->prepare($sql);
@@ -61,6 +66,7 @@ class User {
         return $count > 0;
     }
 
+    //Método para obtener los datos del usuario por su email
     public function getUserByEmail($email){
         $sql = "SELECT user_id, nombre, apellidos, email, password_hash, rol FROM {$this->tabla} WHERE email = :email";
         $stmt = $this->conexion->prepare($sql);
