2e651188fcab086d1ffb5495e6b9a6d7eb2c077a
AntonioMonzo <amonansu@gmail.com>
2025-11-17 22:07:56 +0100

    CREATE user + libro +producto

---
+++ MODEL/user.php
@@ -0,0 +1,71 @@
+<?php 
+require_once '../conexion.php';
+
+class User {
+    private $conexion; //variable privada que se usa para la conexión a la base de datos
+    private $tabla = 'users'; //variable privada que se usa para almacenar el nombre de la tabla
+
+    //Método constructor para inicializar la conexión a la base de datos
+    public function __construct(){ 
+
+        $this->conexion = Conexion::conexionBBDD();
+    }
+
+    public function login($email,$password){
+
+        //Sentencia sql para que se busque al usuario introducido 
+        $sql = "SELECT user_id, nombre, apellidos, email, password_hash, rol FROM {$this->tabla} WHERE email :email";
+
+        //
+        $stmt = $this->conexion->prepare($sql);
+    }
+
+    public function register ($data){
+
+        if($this->emailExists($data['email'])){
+            return ['success' => false, 'message' => 'El correo electrónico ya está registrado.'];
+
+        }
+
+        $sql = "INSERT INTO {$this->tabla} (nombre, apellidos, email, password_hash, fecha_nacimiento, telefono, genero, rol) VALUES (:nombre, :apellidos, :email, :password_hash, :fecha_nacimiento, :telefono, :genero, 'cliente')";
+
+        try{
+            $stmt = $this->conexion->prepare($sql);
+
+            $stmt->bindParam(':nombre', $data['nombre']);
+            $stmt->bindParam(':apellidos', $data['apellidos']);
+            $stmt->bindParam(':email', $data['email']);
+            $passwordHash = password_hash($data['password'], PASSWORD_BCRYPT);
+            $stmt->bindParam(':password_hash', $passwordHash);
+            $stmt->bindParam(':fecha_nacimiento', $data['fecha_nacimiento']);
+            $stmt->bindParam(':telefono', $data['telefono']);
+            $stmt->bindParam(':genero', $data['genero']);
+
+            $stmt->execute();
+
+            return ['success' => true, 'message' => 'Registro exitoso.'];
+
+        } catch (PDOException $e){
+            return ['success' => false, 'message' => 'Error en el registro: ' . $e->getMessage()];
+        }
+    }
+
+
+    public function emailExists($email){
+        $sql = "SELECT COUNT(*) FROM {$this->tabla} WHERE email = :email";
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        $count = $stmt->fetchColumn();
+
+        return $count > 0;
+    }
+
+    public function getUserByEmail($email){
+        $sql = "SELECT user_id, nombre, apellidos, email, password_hash, rol FROM {$this->tabla} WHERE email = :email";
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+}
