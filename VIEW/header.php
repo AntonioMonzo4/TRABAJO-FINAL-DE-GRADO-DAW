1e9f1227cf14b25ca05609c96fac38e8689aa715
AntonioMonzo <amonansu@gmail.com>
2025-11-20 20:27:20 +0100

    MOD header php fallo css

--- VIEW/header.php
+++ VIEW/header.php
@@ -15,7 +15,7 @@
 
     <!-- Añadido CDN de FontAwesome para íconos -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="./VIEW/css/stylesheet.css">
+    <link rel="stylesheet" href="./VIEW/css/style.css">
     <title>Los círculos de Atenea</title>
 </head>
 
