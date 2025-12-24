ad07780777ccea039d0a39de1e9e6c0a75a0d47f
Antonio Monzó <87438455+AntonioMonzo4@users.noreply.github.com>
2025-12-24 11:58:38 +0100

    Delete VIEW directory

--- VIEW/partials/flash.php
+++
@@ -1,5 +0,0 @@
-<?php if (!empty($_SESSION['flash'])): ?>
-<div class="flash <?= $_SESSION['flash']['type'] ?>">
-  <?= htmlspecialchars($_SESSION['flash']['msg']) ?>
-</div>
-<?php unset($_SESSION['flash']); endif; ?>
